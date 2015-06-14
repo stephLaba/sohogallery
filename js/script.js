5d575eac79d9cde2272e741886f52bc5aa2d0867
Steph Laba <steph.laba1@gmail.com>
2015-06-14 11:10:40 -0400

    restored missing files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+
+
 $(function() {
+
+	//array of images
 
 	var images = [
 		".image1",
@@ -11,24 +15,75 @@ $(function() {
 		         return n;
 		      }
 
-		//randomly choose an animation in array
+	//randomly choose an animation in array
 		      function getRandomImage(){
 		       var num = getRandomNumber(images.length);
 		       return images[num];
 		      };
 
-		      console.log(getRandomImage());
+	//set interval of changing z-index
 
 	var interval = setInterval(addZ, 3000);
 
-
+//add z-index to image
 	function addZ(){
 		$(getRandomImage()).toggleClass("z3");
 
 	}
 
+var $galleryImages = $('.thumbnail a');
+
+var wWidth = $(window).innerWidth();
+
+	// console.log($galleryImages);
+	//1. when user clicks on thumbnail show modal box
+	$galleryImages.on('click', function(e){
+		e.preventDefault();
+		var index = $galleryImages.index($(this));
+		//Get full source
+		var src = $(this).data('full');
 
 
+		$('.modalContent').css('background-image',"url(" + src + ")").data('index',index);
 
-	
-});
+		// $('.modalContent').css("width",wWidth);
+
+			// $(window).mousemove(function(e) {
+		 //    $("html, body").scrollTop(function() {
+		 //    	console.log("hello");
+   //      	var h = $(window).height();
+   //      	var y = e.clientY - h / 2;
+   //      	return y;
+
+		 //    });
+
+		    // $(document).on('mousemove', function(e){
+		    // 	var mousePos = (e.clientY/$(window).height())*1000;
+		    // 	$('.modalContent').css('backgroundPosition', mousePos+'px');
+		    // }); 
+
+
+		$('.modalContent').removeClass('closed');
+
+		// });
+		
+	});
+
+
+		$('.selected').removeClass('selected');
+		$(this).addClass('selected');
+		console.log('selected!');
+
+	//2. have modal box get full-size image
+		// the second src (without apostrophes) is the variable
+
+		// var $minHeight = 800;
+		// var $height = $('.modal').height();
+		// if ( $('.modal').$height > $minHeight) {
+		// 	$(this).css("width", "40%");
+		// }
+
+		console.log('imagessssss');
+	});
+
+
